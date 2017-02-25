5c008bc830e120c7157452219f896080c4c1081e
Stacey <staceyastewart@Staceys-MacBook-Pro.local>
2017-02-25 11:34:10 -0500

    created cards in JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,18 +5,35 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardTwo === cardFour) {
-	alert("You found a match!")
+	alert("You found a match!");
 } else if (cardOne === cardThree) {
-	alert("You found a match!")
+	alert("You found a match!");
 } else if (cardOne === cardTwo) {
-	alert("You found a match!")
+	alert("You found a match!");
 } else if (cardThree === cardFour) {
-	alert("You found a match!")
+	alert("You found a match!");
 } else if (cardTwo === cardThree) {
-	alert("You found a match!")
+	alert("You found a match!");
 } else if (cardOne === cardFour) {
-	alert("You found a match!")
+	alert("You found a match!");
 } else {
-	alert("Sorry, try again.")
+	alert("Sorry, try again.");
+};
+
+*/
+
+var cards = [cardOne, cardTwo, cardThree, cardFour];
+
+var boardForGame = document.getElementById('game-board');
+
+function createCards() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		boardForGame.appendChild(cardElement);
+	}
 }
+
+createCards();
